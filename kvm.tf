283971510cfc5bfd9b0e70054c9e12576d829d86
Mike Wright <mike.wright@hashicorp.com>
2024-06-05 11:52:04 -0400

    Update kvm.tf

--- kvm.tf
+++ kvm.tf
@@ -1,73 +1,75 @@
-resource "tls_private_key" "ecdsa-p384-bastion" {
-  algorithm   = "ECDSA"
-  ecdsa_curve = "P384"
-}
-
+# instance the provider
 provider "libvirt" {
-  uri = "qemu+ssh://hero@192.168.165.100/system"
+  uri = "qemu:///system"
 }
 
-module "vm" {
-  source  = "MonolithProjects/vm/libvirt"
-  version = "1.8.0"
-
-  vm_hostname_prefix = "server"
-  vm_count    = 3
-  memory      = "2048"
-  vcpu        = 1
-  pool        = "terra_pool"
-  system_volume = 20
-  dhcp        = true
-  local_admin = "local-admin"
-  ssh_admin   = "ci-user"
-  ssh_private_key = "~/.ssh/id_ed25519"
-  local_admin_passwd = "$6$rounds=4096$xxxxxxxxHASHEDxxxPASSWORD"
-  ssh_keys    = [
-    "ssh-ed25519 AAAAxxxxxxxxxxxxSSHxxxKEY example",
-    ]
-  bastion_host = "10.0.0.1"
-  bastion_user = "admin"
-  bastion_ssh_private_key = tls_private_key.ecdsa-p384-bastion.private_key_pem
-  time_zone   = "CET"
-  os_img_url  = "file:///home/edge-admin/ubuntu-20.04-server-cloudimg-amd64.img"
-  xml_override = {
-      hugepages = true,
-      usb_controllers = [
-        {
-          model = "qemu-xhci"
-        }
-      ],
-      usb_devices = [
-        {
-          vendor = "0x0bc2",
-          product = "0xab28"
-        }
-      ]
-      pci_devices_passthrough = [
-        {
-          src_domain = "0x0000",
-          src_bus    = "0xc1",
-          src_slot   = "0x00",
-          src_func   = "0x0",
-          dst_domain = "0x0000",
-          dst_bus    = "0x00",
-          dst_slot   = "0x08"
-          dst_func   = "0x0"
-        },
-        {
-          src_domain = "0x0000",
-          src_bus    = "0xc1",
-          src_slot   = "0x00",
-          src_func   = "0x1",
-          dst_domain = "0x0000",
-          dst_bus    = "0x00",
-          dst_slot   = "0x09"
-          dst_func   = "0x0"
-        }
-      ]      
-    }
+resource "libvirt_pool" "ubuntu" {
+  name = "ubuntu"
+  type = "dir"
+  path = "/tmp/terraform-provider-libvirt-pool-ubuntu"
 }
 
-output "ip_addresses" {
-  value = module.nodes
+# We fetch the latest ubuntu release image from their mirrors
+resource "libvirt_volume" "ubuntu-qcow2" {
+  name   = "ubuntu-qcow2"
+  pool   = libvirt_pool.ubuntu.name
+  source = "https://cloud-images.ubuntu.com/releases/xenial/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
+  format = "qcow2"
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/cloud_init.cfg")
+}
+
+data "template_file" "network_config" {
+  template = file("${path.module}/network_config.cfg")
+}
+
+# for more info about paramater check this out
+# https://github.com/dmacvicar/terraform-provider-libvirt/blob/master/website/docs/r/cloudinit.html.markdown
+# Use CloudInit to add our ssh-key to the instance
+# you can add also meta_data field
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name           = "commoninit.iso"
+  user_data      = data.template_file.user_data.rendered
+  network_config = data.template_file.network_config.rendered
+  pool           = libvirt_pool.ubuntu.name
+}
+
+# Create the machine
+resource "libvirt_domain" "domain-ubuntu" {
+  name   = "ubuntu-terraform"
+  memory = "512"
+  vcpu   = 1
+
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
+
+  network_interface {
+    network_name = "default"
+  }
+
+  # IMPORTANT: this is a known bug on cloud images, since they expect a console
+  # we need to pass it
+  # https://bugs.launchpad.net/cloud-images/+bug/1573095
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = libvirt_volume.ubuntu-qcow2.id
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
 }
